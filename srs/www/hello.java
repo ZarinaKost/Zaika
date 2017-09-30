153e80d774fd5be3539e737bdcbfc848ec43e800
Zaya <zarina.kost11@gmail.com>
2017-10-01 00:15:04 +0600

    experiment1 бурагында Hello.java жане MyMath.java файлдарына озгеристер жасалады.

--- srs/www/hello.java
+++ srs/www/hello.java
@@ -5,5 +5,13 @@ public class hello {
         System.out.println("Hello World!");
         System.out.println("Бұл зертханалық жұмыстың мақсаты");
         System.out.println("GitHub-пен жұмыс жасап үйрену.");
-    }
-}
+
+        int a = MyMath.kvadrat(a:5);
+        int b = MyMath.kvadrat(a:5);
+
+        System.out.println("a= " +a);
+        System.out.println("b= " +b);
+
+        System.out.println(MyMath.max(a:8,b:9)) ;
+        }
+        }
