3f11caa98234caf8cff5e4a5eeba3f24572a89a0
Zaya <zarina.kost11@gmail.com>
2017-09-30 22:09:33 +0600

    aaa

--- srs/www/hello.java
+++ srs/www/hello.java
@@ -3,5 +3,7 @@ package www;
 public class hello {
     public static void main (String [] args){
         System.out.println("Hello World!");
+        System.out.println("Бұл зертханалық жұмыстың мақсаты");
+        System.out.println("GitHub-пен жұмыс жасап үйрену.");
     }
 }
